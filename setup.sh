43392bc49633e3feee85d9c30f07243ae7830871
AnnikaLindh <code.annikalindh@gmail.com>
2021-05-07 13:58:37 +0100

    Updated the google drive links to their new locations.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Creating directories..."
 mkdir cg_checkpoints
 
 echo "Downloading preprocessed data..."
-gdown https://drive.google.com/uc?id=1aTHBFW0XFDWsjSc4SOETapszi5pLDctc
+gdown https://drive.google.com/uc?id=18fSwjw_F2aL-nDpQouEJ9Mh_cC12SyW9
 
 echo "Unpacking preprocessed data..."
 unzip -q data.zip
